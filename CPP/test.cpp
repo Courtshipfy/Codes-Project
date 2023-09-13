c08b13a95b559b4a6f07c8677dcfa4ff169e3e6f
Courtshipfy <courtship_fy@courtship.top>
2023-09-13 11:05:28 +0800

    STL Project

---
+++ CPP/test.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+template <typename T>
+class print
+{
+public:
+    void operator()(const T& elem) 
+    { std::cout<< elem << " ";}
+};
+
+int main()
+{
+    int ia[6] = {0,1,2,3,4,5};
+    std::vector<int> iv(ia,ia + 6);
+
+    for_each(iv.begin(),iv.end(),print<int>());
+}
