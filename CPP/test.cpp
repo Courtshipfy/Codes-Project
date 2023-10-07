eb1355c91ab16ae3a1d4d0ab1bc07b5b1fa9da88
Courtshipfy <courtship_fy@courtship.top>
2023-10-07 20:24:40 +0800

    xxxx

--- CPP/test.cpp
+++ CPP/test.cpp
@@ -10,6 +10,17 @@ public:
     { std::cout<< elem << " ";}
 };
 
+template <typename T1,typename T2 = T1>
+auto sum(T1 t1,T2 t2) -> decltype(t1 + t2)
+{
+    return t1 + t2;
+}
+
+auto func(int i) -> int(*)[10]
+{
+    return 0;
+}
+
 int main()
 {
     int ia[6] = {0,1,2,3,4,5};
@@ -17,5 +28,19 @@ int main()
 
     for_each(iv.begin(),iv.end(),print<int>());
 
-    
+    bool x = true;
+    //x = x && false;
+    //x &= false;
+    std::cout << x << std::endl;
+    std::cout << (1 << 20) << std::endl;
+
+    auto xxxx = func(2);
+    std::cout << xxxx << std::endl;
+
+    std::cout << sum(1,2.0) << std::endl;
+
+    auto lambdafunc = [](int a,int b) -> double
+    {
+        return static_cast<double>(a) / b;
+    };
 }
